d29252104aad2190b3fb5c7198c9f065518873ce
Gabriel Castro <gabrielcssg@gmail.com>
2019-06-12 18:40:04 -0300

    Adicionado a busca de venda por data

--- venda/urls.py
+++ venda/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import nova_venda,listar_vendas
-
+from .views import nova_venda, listar_vendas, listar_vendas_por_data
 
 urlpatterns = [
-    path('', listar_vendas,name = 'url_lista_vendas'),
-    path('nova/',nova_venda,name = 'url_nova_venda')
+    path('', listar_vendas, name='url_lista_vendas'),
+    path('nova/', nova_venda, name='url_nova_venda'),
+    path('buscar_vendas_por_data/', listar_vendas_por_data, name='url_buscar_vendas_por_data')
 
 ]
